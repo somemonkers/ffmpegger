39b8a9ca7537ea1164c6e3aa668541b9e415872c
Aaron C <50064042+monke0192@users.noreply.github.com>
2022-10-10 17:17:19 +1100

    thing

--- ffmpegging.sh
+++ ffmpegging.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-ffmpeg -i "$@" -ar 8000 -r 6 -b:a 8k -af volume=90dB -c:a flac -strict -2 -b:v 6k -s 280x144 out.mp4
+output=$2
+[ -z "$2" ] && output="out.mp4"
+ffmpeg -i "$1" -ar 8000 -r 6 -b:a 8k -af volume=90dB -c:a flac -strict -2 -b:v 6k -s 280x144 "$output"
