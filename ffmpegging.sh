3f74fb0d42677796ff66b11fd9fda9577d394f56
Glint <85619855+glintty@users.noreply.github.com>
2022-10-11 21:18:39 +1100

    amogu uh i made stuff bloat uh sandjbwjncxhw

--- ffmpegging.sh
+++ ffmpegging.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-input=$1
 [ -z "$1" ] && echo "err: Input file not found! Try ffmpegging.sh <input> <output>" && exit
 output=$2
 [ -z "$2" ] && output="out.mp4"
-ffmpeg -i "$input" -ar 8000 -r 6 -b:a 8k -af volume=90dB -c:a flac -strict -2 -b:v 6k -s 280x144 "$output"
+ffmpeg -i "$1" -ar 8000 -r 6 -b:a 8k -af volume=90dB -c:a flac -strict -2 -b:v 6k -s 280x144 "$output"
